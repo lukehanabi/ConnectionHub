07ca2b845a7ad5e417c617304ca1dd84e774f14b
federicofernan <federicofernan@192.168.1.100>
2014-11-15 18:36:31 -0300

    diff

--- ConnectionHub/src/ConnectionHub/Main/ConnectionHub.java
+++ ConnectionHub/src/ConnectionHub/Main/ConnectionHub.java
@@ -8,7 +8,7 @@ public class ConnectionHub {
 	public static void main(String[] args) {
 		ConfigFile fileConfig = new ConfigFile();
 		fileConfig.setConfigFileName("config.properties");
-	    //get folder configuration
+	    //get folder configuration DIFF
 		String configFilesPath = fileConfig.getProperty("SourceConfigPath");
 		AppFile appFile = new AppFile();
 		//Hola Fede
